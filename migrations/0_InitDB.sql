68556351ab20a64a3426bd85326a12eba6d8d710
STcraft <stefanvv@protonmail.com>
2023-01-25 15:29:25 +0100

    Create first sql file

---
+++ migrations/0_InitDB.sql
@@ -0,0 +1,29 @@
+CREATE TABLE file ( 
+	id                   INTEGER NOT NULL  PRIMARY KEY  ,
+	parent               INTEGER     ,
+	name                 TEXT NOT NULL    ,
+	title                TEXT NOT NULL    ,
+	description          TEXT     ,
+	mimeType             TEXT NOT NULL    ,
+	data                 BLOB     ,
+	metadata             TEXT     ,
+	createdOn            INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP   ,
+	modifiedOn           INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP   ,
+	FOREIGN KEY ( parent ) REFERENCES file( id )  ,
+	CHECK ( json_valid(metadata) )
+ );
+
+CREATE TABLE icon ( 
+	name                 TEXT NOT NULL  PRIMARY KEY  ,
+	path                 TEXT NOT NULL    
+ );
+
+CREATE TABLE userLink ( 
+	id                   INTEGER NOT NULL  PRIMARY KEY  ,
+	title                TEXT NOT NULL    ,
+	subTitle             TEXT     ,
+	url                  TEXT     ,
+	icon                 TEXT     ,
+	FOREIGN KEY ( icon ) REFERENCES icon( name )  
+ );
+
