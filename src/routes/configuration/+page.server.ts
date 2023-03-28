280051b76518a233728039c2bc9f0bdaf6ed97c1
STcraft <stefanvv@protonmail.com>
2023-03-28 22:08:32 +0200

    Update packages, remove unused files

--- src/routes/configuration/+page.server.ts
+++ src/routes/configuration/+page.server.ts
@@ -1,12 +1,6 @@
-import { db } from '$lib/server/Db';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async () => {
 	return {};
 }) satisfies PageServerLoad;
 
-export const actions: Actions = {
-	test: async (event) => {
-		return { success: true };
-	}
-};
